9680ca2e247d628ddb866b305d569a5ba4490ef9
Daniel <daniel.studders@rs-components.com>
2020-05-27 15:19:06 +0100

    css work WIP

---
+++ spotcheck-ada-part-b/src/map.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import Redirect from "./index";
+import "./map.css";
+
+import Add from "./add";
+import Profile from "./profile";
+
+import profile from "./img/profile.png";
+import map from "./img/map.png";
+import add from "./img/add.png";
+
+class Map extends Component {
+  render() {
+    return (
+      <div className="Map">
+
+        <div className="header">
+          <h1 className="header-title">Spotchecker </h1>
+        </div>
+
+        <div className="main-page-map">
+
+        </div>
+
+        <div className="footer">
+
+          <div className="footer-icon">
+            <img
+              src={map}
+              onClick={() => {
+                Redirect(<Map />);
+              }}
+              alt="map"
+            ></img>
+          </div>
+
+          <div className="footer-icon">
+            <img
+              src={add}
+              onClick={() => {
+                Redirect(<Add />);
+              }}
+              alt="add"
+            ></img>
+          </div>
+
+          <div className="footer-icon">
+            <img
+              src={profile}
+              onClick={() => {
+                Redirect(<Profile />);
+              }}
+              alt="profile"
+            ></img>
+          </div>
+
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default Map;
--- spotcheck-ada-part-b/src/profile.js
+++ spotcheck-ada-part-b/src/profile.js
@@ -1,25 +1,37 @@
 import React, { Component } from "react";
 import Redirect from "./index";
 
-import App from "./App";
 import Add from "./add";
-import Profile from "./profile";
+import Map from "./map";
 
 import profile from "./img/profile.png";
 import map from "./img/map.png";
 import add from "./img/add.png";
 
-class Map extends Component {
+class Profile extends Component {
   render() {
     return (
-      <div className="Map">
+      <div className="Event">
 
         <div className="header">
           <h1 className="header-title">Spotchecker </h1>
         </div>
 
-        <div className="main-page-map">
+        <div className="subpage-header">
+          <div className="subpage-header-title">Profile</div>
+        </div>
 
+        <div className="profile-top-bar">
+
+          <div className="customer-info">
+            <p>
+              {" "}
+              <b>Name: </b>
+              {/* <br></br> {this.props.user.displayName} <br></br> */}
+              <b>Age:</b> <br></br> 21 <br></br>
+              {/* <b> Email:</b> <br></br> {this.props.user.email} */}
+            </p>
+          </div>
         </div>
 
         <div className="footer">
@@ -61,4 +73,4 @@ class Map extends Component {
   }
 }
 
-export default Map;
+export default Profile;

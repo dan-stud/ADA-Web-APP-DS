c72527b988637978678d221764f45079c526a8ca
Daniel <daniel.studders@rs-components.com>
2020-05-28 13:40:07 +0100

    more UI and css

--- spotcheck-ada-part-b/src/add.js
+++ spotcheck-ada-part-b/src/add.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Redirect from "./index";
+import "./add.css";
 
 import Map from "./map";
 import Profile from "./profile";
@@ -7,9 +8,12 @@ import Profile from "./profile";
 import profile from "./img/profile.png";
 import map from "./img/map.png";
 import add from "./img/add.png";
+import insert from "./img/insert-spot.png"
 
 class Add extends Component {
   render() {
+
+
     return (
       <div className="Event">
 
@@ -18,8 +22,40 @@ class Add extends Component {
         </div>
 
         <div className="subpage-header">
-          <div className="subpage-header-title">Add New Spot</div>
+          <div className="subpage-header-title">New Spot</div>
         </div>
+
+        <form>
+          <div className="main-content-contianer">
+
+            <div className="image-container">
+              <img
+                src={insert}
+                alt="insert spot img here"
+              ></img>
+              <p> Add Image</p>
+            </div>
+
+            <div className="spot-info">
+              <div className="info">
+              <label for="sdesc"> Spot Description: </label><br></br>
+                <input type="text" id="sdesc" name="sdesc" value="add"/>
+              </div>
+
+              <div className="info">
+                <b> Spot Type </b>
+                
+              </div>
+
+              <div className="info">
+                <b> Spot Difficulty Level </b>
+              </div>
+
+            </div>
+
+          </div>
+
+        </form>
 
         <div className="footer">
 
@@ -52,7 +88,7 @@ class Add extends Component {
               alt="profile"
             ></img>
           </div>
-          
+
         </div>
 
       </div>
--- spotcheck-ada-part-b/src/profile.js
+++ spotcheck-ada-part-b/src/profile.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Redirect from "./index";
+import "./profile.css";
 
 import Add from "./add";
 import Map from "./map";
@@ -7,6 +8,7 @@ import Map from "./map";
 import profile from "./img/profile.png";
 import map from "./img/map.png";
 import add from "./img/add.png";
+import profileDefault from "./img/profile-default.png";
 
 class Profile extends Component {
   render() {
@@ -17,21 +19,35 @@ class Profile extends Component {
           <h1 className="header-title">Spotchecker </h1>
         </div>
 
-        <div className="subpage-header">
-          <div className="subpage-header-title">Profile</div>
-        </div>
-
         <div className="profile-top-bar">
+
+          <div className="user-img-container">
+            <img
+            className="user-profile-img"
+              src={profileDefault}
+              alt="user img"
+            ></img>
+            <p> Add Image</p>
+          </div>
 
           <div className="customer-info">
             <p>
-              {" "}
-              <b>Name: </b>
-              {/* <br></br> {this.props.user.displayName} <br></br> */}
-              <b>Age:</b> <br></br> 21 <br></br>
-              {/* <b> Email:</b> <br></br> {this.props.user.email} */}
+            <b> Name: </b> <p> Joe Flynn</p>
+            <b> Email: </b> <p> Markdid911@gmail.com</p>
+            <b> Bio: </b> <p> insert Bio Here </p>
             </p>
           </div>
+
+          {/* <div className="customer-info">
+            <p>
+              {" "}
+              <b>Name: </b>
+              <br></br> {this.props.user.displayName} <br></br>
+              <b>Age:</b> <br></br> 21 <br></br>
+              <b> Email:</b> <br></br> {this.props.user.email}
+            </p>
+          </div> */}
+
         </div>
 
         <div className="footer">
